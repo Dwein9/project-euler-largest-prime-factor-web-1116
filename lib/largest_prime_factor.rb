066b7a5bb0cf9dce623ebb9f9c665a9ff59ce9c2
David Weinstein <txea@Davids-MacBook-Air.local>
2017-02-03 10:30:35 -0500

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,17 @@
 # Enter your procedural solution here!
+require 'prime'
+
+def largest_prime_factor(num)
+
+  factor = 2
+  prime_factor = 0
+
+  while factor <= num
+    if num % factor == 0 && Prime.prime?(factor)
+      num /= factor
+      prime_factor = factor
+    end
+    factor += 1
+  end
+  prime_factor
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,24 @@
 # Enter your object-oriented solution here!
+require 'prime'
+
+class LargestPrimeFactor
+
+def initialize(num)
+  @num = num
+end
+
+
+  def number
+    factor = 2
+    prime_factor = 0
+
+    while factor <= @num
+      if @num % factor == 0 && Prime.prime?(factor)
+        @num /= factor
+        prime_factor = factor
+      end
+      factor += 1
+    end
+    prime_factor
+  end
+end
